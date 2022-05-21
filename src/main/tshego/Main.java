e5dd8a3df064abcf73acd176b050941b061ce10e
Tshego Moloto <tmoloto021@student.wethinkcode.co.za>
2022-05-21 06:38:21 +0200

    Learning Encapsulation

--- src/main/tshego/Main.java
+++ src/main/tshego/Main.java
@@ -3,10 +3,6 @@ package main.tshego;
 public class Main {
     public static void main(String[] args) {
         Car myCar = new Car("Nissan", "GT-R R35", 2017);
-        StringBuilder newString = new StringBuilder();
-        newString.append("Hello");
-        newString.insert(1, "a");
-        System.out.println(newString.charAt(1));
         myCar.printCarMake();
     }
 }
